a2bace4a7fe556c023b8d878257ca92b0133c39d
Yanxxx <liyan.career@gmail.com>
2023-01-13 09:55:11 -0500

    remove duplicate helmet setups

--- src/scripts/vrserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH://home/robot/.local/share/Steam/steamapps/common/SteamVR/bin/linux64
-/home/robot/.local/share/Steam/steamapps/common/SteamVR/bin/linux64//vrserver  --keepalive
-
